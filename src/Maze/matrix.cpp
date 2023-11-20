ab91c3b62861db0b80b4790a552ed4ac7ad58e7a
Sabina <evetteis@student.21-school.ru>
2023-11-20 15:46:51 +0300

    fix generator

--- src/Maze/matrix.cpp
+++ src/Maze/matrix.cpp
@@ -2,6 +2,8 @@
 #include <cstdlib> // for rand function
 #include <cstdlib>   // Подключаем библиотеку для работы с функциями, связанными с генерацией случайных чисел
 #include <ctime>
+#include <algorithm>
+#include <random>
 
 Matrix::Matrix(int numRows, int numCols) : rows(numRows), cols(numCols) {
     vertical.resize(rows + 1, std::vector<int>(cols, 0));
@@ -10,7 +12,7 @@ Matrix::Matrix(int numRows, int numCols) : rows(numRows), cols(numCols) {
     sets.resize(rows, std::vector<int>(cols, -1));
     for (int i = 0; i < rows; ++i) {
         for (int j = 0; j < cols; ++j) {
-            sets[i][j] = i * cols + j;
+            sets[i][j] = j +1;
             horizontal[i][j] = 1;
             std::cout << sets[i][j] << " ";
         }
@@ -39,6 +41,17 @@ int Matrix::getSetValue(int row, int col) const {
     return sets[row][col];
 }
 
+
+std::vector<int> generateVector(int size) {
+    // Создаем вектор, заполненный единицами
+    std::vector<int> result(size, 1);
+
+    // Добавляем один ноль в рандомную позицию
+    result[rand() % size] = 0;
+
+    return result;
+}
+
 void Matrix::generateMaze() {
     srand(static_cast<unsigned>(time(0)));
 
@@ -51,15 +64,59 @@ void Matrix::generateMaze() {
             if (j != 0 && vertical[i][j-1] == 0)
             {
                 sets[i][j] = sets[i][j-1]; // предыдущему переприсваиваю номер множества, если стенки нет
+            }
+
+            if (i == rows - 1) // для ласт ряда
+            {
+                sets[i][j] = sets[i-1][j];
+
+                if (sets[i][j-1] == sets[i][j] && j > 0) {
+                    vertical[i][j] = rand() & 1;
+                } else if (sets[i][j-1] != sets[i][j] && j > 0) {
+                    sets[i][j] = sets[i][j-1];
+                    vertical[i][j] = 0;
+                }
 
             }
+
             std::cout << sets[i][j] << " ";
         }
 
-        checkSetInRow(i);
+        for (int j = 0; j < cols; )
+        {
+            int size_this_set = 0, itterator = 0;
+
+            while (sets[i][size_this_set+1+j] == sets[i][size_this_set+j]) {
+                size_this_set++;
+            }
+
+            if (size_this_set != 0) {
+                std::vector<int> values = generateVector(size_this_set+1);
+                std::cout << " vector: ";
+                for (int v : values) {
+                    std::cout << "[" << i << "][" << j << "] - " << v  << ", ";
+                    horizontal[i][itterator+j] = v;
+                    j++;
+                }
+                std::cout << "; " << j << " <- j ";
+            } else if (sets[i][j+1] != sets[i][j] && j != cols-1) {
+                std::cout << " мне похуй снова j = " << j << "; ";
+                horizontal[i][j] = 0;
+                j++;
+            } else if (sets[i][j-1] != sets[i][j] && j == cols-1) {
+                std::cout << " мне похуй j = " << j << "; ";
+                horizontal[i][j] = 0;
+                j++;
+            } else {
+                std::cout << " тут else j = " << j << "; ";
+                horizontal[i][j] = 1;
+                j++;
+            }
+        }
 
         std::cout << "\n";
-        checkHorizontalInRow(i);
+//        if (i != rows -1)
+//            checkHorizontalInRow(i);
     }
 }
 
@@ -90,33 +147,33 @@ void Matrix::checkSetInRow(int i) {
 
 void Matrix::checkHorizontalInRow(int i) {
     std::vector<int> uniqueSets;
+    for (int j = 0; j < cols; j++) {
+        if (std::find(uniqueSets.begin(), uniqueSets.end(), sets[i][j]) == uniqueSets.end()) {
+            uniqueSets.push_back(sets[i][j]);
+        }
+    }
+
+    // перебор по уникальным номерам сета
+    for (int setNumber : uniqueSets) {
+        // индексы ячеек с текущим номером сета
+        std::vector<int> cellsWithSet;
         for (int j = 0; j < cols; j++) {
-            if (std::find(uniqueSets.begin(), uniqueSets.end(), sets[i][j]) == uniqueSets.end()) {
-                uniqueSets.push_back(sets[i][j]);
+            if (sets[i][j] == setNumber) {
+                cellsWithSet.push_back(j);
             }
         }
 
-        // перебор по уникальным номерам сета
-        for (int setNumber : uniqueSets) {
-            // индексы ячеек с текущим номером сета
-            std::vector<int> cellsWithSet;
-            for (int j = 0; j < cols; j++) {
-                if (sets[i][j] == setNumber) {
-                    cellsWithSet.push_back(j);
-                }
-            }
-
-            // если только одна ячейка с определенным номером (стенки вокруг), удаляем нижнюю стенку
-            if (cellsWithSet.size() == 1) {
-                int singleCell = cellsWithSet[0];
-                horizontal[i][singleCell] = 0;
-            }
-            // когда несколько ячеек с одинаковым номером, удаляем рандомно нижнюю стенку
-            else if (cellsWithSet.size() > 1) {
-                int randomIndex = rand() % cellsWithSet.size();
-                int randomCell = cellsWithSet[randomIndex];
-
-                horizontal[i][randomCell] = 0;
-            }
+        // если только одна ячейка с определенным номером (стенки вокруг), удаляем нижнюю стенку
+        if (cellsWithSet.size() == 1) {
+            int singleCell = cellsWithSet[0];
+            horizontal[i][singleCell] = 0;
         }
+        // когда несколько ячеек с одинаковым номером, удаляем рандомно нижнюю стенку
+        else if (cellsWithSet.size() > 1) {
+            int randomIndex = rand() % cellsWithSet.size();
+            int randomCell = cellsWithSet[randomIndex];
+
+            horizontal[i][randomCell] = 0;
+        }
+    }
 }

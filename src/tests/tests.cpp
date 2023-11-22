e86b732cb788d465f2727a29cfe33839fe3bad52
Hsiu Ayesha <hsiuayes@si-f3.kzn.21-school.ru>
2023-11-22 19:39:45 +0300

    tests

--- src/tests/tests.cpp
+++ src/tests/tests.cpp
@@ -16,6 +16,130 @@ TEST(MatrixTest, GenerateMaze) {
     }
 }
 
+TEST(MatrixTest, GenerateMaze1) {
+  Matrix matrix(3, 3);
+  matrix.generateMaze();
+
+  EXPECT_EQ(matrix.getRows(), 3);
+  EXPECT_EQ(matrix.getColumns(), 3);
+
+  EXPECT_EQ(matrix.getVerticalValue(2, 1), 0);
+  EXPECT_EQ(matrix.getHorizontalValue(2, 2), 0);
+
+  for (int i = 0; i < matrix.getRows(); ++i) {
+    for (int j = 0; j < matrix.getColumns(); ++j) {
+      if (!(i == 2 && j == 1) && !(i == 2 && j == 2)) {
+        EXPECT_TRUE(matrix.getVerticalValue(i, j) == 0 || matrix.getVerticalValue(i, j) == 1);
+        EXPECT_TRUE(matrix.getHorizontalValue(i, j) == 0 || matrix.getHorizontalValue(i, j) == 1);
+      }
+    }
+  }
+}
+
+TEST(MatrixTest, GenerateMaze3) {
+        Matrix matrix(2, 2);
+        matrix.generateMaze();
+
+        EXPECT_EQ(matrix.getRows(), 2);
+        EXPECT_EQ(matrix.getColumns(), 2);
+
+        for (int i = 0; i < matrix.getRows(); ++i) {
+            for (int j = 0; j < matrix.getColumns(); ++j) {
+                EXPECT_TRUE(matrix.getVerticalValue(i, j) == 0 || matrix.getVerticalValue(i, j) == 1);
+                EXPECT_TRUE(matrix.getHorizontalValue(i, j) == 0 || matrix.getHorizontalValue(i, j) == 1);
+            }
+        }
+    }
+
+    TEST(MatrixTest, GenerateMaze4) {
+        Matrix matrix(4, 6);
+        matrix.generateMaze();
+
+        EXPECT_EQ(matrix.getRows(), 4);
+        EXPECT_EQ(matrix.getColumns(), 6);
+
+        for (int i = 0; i < matrix.getRows(); ++i) {
+            for (int j = 0; j < matrix.getColumns(); ++j) {
+                EXPECT_TRUE(matrix.getVerticalValue(i, j) == 0 || matrix.getVerticalValue(i, j) == 1);
+                EXPECT_TRUE(matrix.getHorizontalValue(i, j) == 0 || matrix.getHorizontalValue(i, j) == 1);
+            }
+        }
+    }
+
+   TEST(MatrixTest, GenerateMaze5) {
+        Matrix matrix(1, 1);
+        matrix.generateMaze();
+
+        EXPECT_EQ(matrix.getRows(), 1);
+        EXPECT_EQ(matrix.getColumns(), 1);
+
+        EXPECT_TRUE(matrix.getVerticalValue(0, 0) == 0 || matrix.getVerticalValue(0, 0) == 1);
+        EXPECT_TRUE(matrix.getHorizontalValue(0, 0) == 0 || matrix.getHorizontalValue(0, 0) == 1);
+    }
+
+    TEST(MatrixTest, GenerateMaze6) {
+        Matrix matrix(3, 3);
+        matrix.generateMaze();
+
+        EXPECT_EQ(matrix.getRows(), 3);
+        EXPECT_EQ(matrix.getColumns(), 3);
+
+        EXPECT_EQ(matrix.getVerticalValue(2, 1), 0);
+        EXPECT_EQ(matrix.getHorizontalValue(2, 2), 0);
+
+        for (int i = 0; i < matrix.getRows(); ++i) {
+            for (int j = 0; j < matrix.getColumns(); ++j) {
+                if (!(i == 2 && j == 1) && !(i == 2 && j == 2)) {
+                    EXPECT_TRUE(matrix.getVerticalValue(i, j) == 0 || matrix.getVerticalValue(i, j) == 1);
+                    EXPECT_TRUE(matrix.getHorizontalValue(i, j) == 0 || matrix.getHorizontalValue(i, j) == 1);
+                }
+            }
+        }
+    }
+
+    TEST(MatrixTest, GenerateMaze9) {
+        Matrix matrix(3, 5);
+        matrix.generateMaze();
+
+        EXPECT_EQ(matrix.getRows(), 3);
+        EXPECT_EQ(matrix.getColumns(), 5);
+
+        for (int i = 0; i < matrix.getRows(); ++i) {
+            for (int j = 0; j < matrix.getColumns(); ++j) {
+                EXPECT_TRUE(matrix.getVerticalValue(i, j) == 0 || matrix.getVerticalValue(i, j) == 1);
+                EXPECT_TRUE(matrix.getHorizontalValue(i, j) == 0 || matrix.getHorizontalValue(i, j) == 1);
+            }
+        }
+    }
+
+    TEST(MatrixTest, GenerateMaze10) {
+        Matrix matrix(100, 100);
+        matrix.generateMaze();
+
+        EXPECT_EQ(matrix.getRows(), 100);
+        EXPECT_EQ(matrix.getColumns(), 100);
+
+        for (int i = 0; i < matrix.getRows(); ++i) {
+            for (int j = 0; j < matrix.getColumns(); ++j) {
+                EXPECT_TRUE(matrix.getVerticalValue(i, j) == 0 || matrix.getVerticalValue(i, j) == 1);
+                EXPECT_TRUE(matrix.getHorizontalValue(i, j) == 0 || matrix.getHorizontalValue(i, j) == 1);
+            }
+        }
+    }
+
+    // TEST(MatrixTest, GenerateMaze) {
+    //     Matrix matrix(55, 60);
+
+    //     EXPECT_THROW(matrix.generateMaze(), std::runtime_error);
+
+    //     try {
+    //         matrix.generateMaze();
+    //     } catch (const std::runtime_error& e) {
+    //         EXPECT_STREQ(e.what(), "Invalid matrix size: maximum allowed size is 50x50");
+    //     }
+    // }
+  
+
 TEST(MatrixTest, GenerateMaze2) {
     int numRows = 10;
     int numCols = 10;

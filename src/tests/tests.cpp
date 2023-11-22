33341b2faf918e11f806b22247c6de0ad5584ea8
Sabina <evetteis@student.21-school.ru>
2023-11-22 19:20:22 +0300

    test2

--- src/tests/tests.cpp
+++ src/tests/tests.cpp
@@ -16,6 +16,32 @@ TEST(MatrixTest, GenerateMaze) {
     }
 }
 
+TEST(MatrixTest, GenerateMaze2) {
+    int numRows = 10;
+    int numCols = 10;
+    Matrix matrix(numRows, numCols);
+    matrix.generateMaze();
+
+    bool is_right = false;
+    bool is_bottom = false;
+
+    for (int i = 0; i < numRows; i++) {
+        for (int j = 0; j < numCols; j++) {
+            if (matrix.getVerticalValue(i, j) == 1  || matrix.getVerticalValue(i, j) == 0) {
+                is_bottom = true;
+            }
+            if (matrix.getHorizontalValue(i, j) == 1 || matrix.getHorizontalValue(i, j) == 0) {
+                is_right = true;
+            }
+        }
+    }
+
+    ASSERT_TRUE(is_bottom);
+    ASSERT_TRUE(is_right);
+    std::string path = "examples/generatedInTest.txt";
+    matrix.saveMaze(path);
+}
+
 TEST(MatrixTest, LoadMazeSuccess) {
     Matrix matrix;
     ASSERT_NO_THROW(matrix.loadMaze("examples/4x4.txt"));
@@ -29,6 +55,13 @@ TEST(MatrixTest, LoadMazeSuccess) {
 TEST(MatrixTest, LoadMazeInvalidFormat) {
     Matrix matrix;
     ASSERT_THROW(matrix.loadMaze("examples/invalid_format_maze.txt"), std::runtime_error);
+    ASSERT_EQ(matrix.getRows(), 0);
+    ASSERT_EQ(matrix.getColumns(), 0);
+}
+
+TEST(MatrixTest, LoadMazeInvalidData) {
+    Matrix matrix;
+    ASSERT_THROW(matrix.loadMaze("examples/invalid_data_maze.txt"), std::runtime_error);
     ASSERT_EQ(matrix.getRows(), 0);
     ASSERT_EQ(matrix.getColumns(), 0);
 }
